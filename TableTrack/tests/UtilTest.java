a7a5686a7f138f8be888c8a943d1e321ec929a2d
Ramanpreet Singh <rsa149@uregina.ca>
2024-07-19 16:38:56 -0600

    Initialize test files

---
+++ TableTrack/tests/UtilTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class UtilTest {
+
+    @Test
+    public void testGetDateFromUser_ValidDate() {
+      
+    }
+}
+
